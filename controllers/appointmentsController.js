8f9712321c5c05eccf4678691c954349496a44a1
Brian Lee <48812505+BrianYLee@users.noreply.github.com>
2024-08-28 18:21:55 +0800

    add checkin and checkout

--- controllers/appointmentsController.js
+++ controllers/appointmentsController.js
@@ -21,3 +21,95 @@ exports.getAppointments = (req, res) => {
         res.json(results);
     });
 };
+
+exports.postCheckIn = (req, res) => {
+    const body = req.body;
+    console.log('got checkin post request');
+    console.log(body);
+    if (!body || !body.openId || !body.apmtId) {
+        return res.status(400).json({ error: 'bad request payload'});
+    }
+    
+    // queries
+    const employeeIdQuery = 'SELECT id FROM employees WHERE wechat_open_id = ?';
+    const apmtQuery = 'UPDATE appointments SET checked_in = ? WHERE id = ?';
+    const checkInQuery = 'INSERT INTO check_ins (appointment_id, employee_id) VALUES (?, ?)';
+    
+    // results
+    pool.query(employeeIdQuery, [body.openId], (err1, results1) => {
+        if (err1) {
+            console.log('db error while fetching employee ID ' + body.openId);
+            return res.status(500).json({ error: 'db error while fetching employee ID' });
+        }
+        if (results1.length == 0) {
+            console.log('no employee found with openid ' + body.openId);
+            return res.status(500).json({ error: 'no employee foun with given openid' });
+        }
+        if (results1.length > 1) {
+            console.log('something is srsly fucked. why are there multiple employees for openid ' + body.openId);
+            console.log(results1);
+            return res.status(500).json({ error: 'something is srsly fucked. why are there multiple employees for the given openid '});
+
+        }
+        const employeeId = results1[0].id;
+        pool.query(apmtQuery, [true, body.apmtId], (err2, results2) => {
+            if (err2) {
+                console.log('db error while updating appointment id ' + body.apmtId);
+                return res.status(500).json({ error: `db error while updating appointment id ${body.apmtId}` });
+            }
+            pool.query(checkInQuery, [body.apmtId, employeeId], (err3, results3) => {
+                if (err3) {
+                    console.log(`db error while creating checkin for apmtId ${body.apmtId}`);
+                    return res.status(500).json({ error: `db error while creating checkin for apmtId ${body.apmtId}` });
+                }
+                res.status(200).json({ message: 'appointment checked in'});
+            });
+        });
+    });
+};
+
+exports.postCheckOut = (req, res) => {
+    const body = req.body;
+    console.log('got check-out post request');
+    console.log(body);
+    if (!body || !body.openId || !body.apmtId) {
+        return res.status(400).json({ error: 'bad request payload'});
+    }
+    
+    // queries
+    const employeeIdQuery = 'SELECT id FROM employees WHERE wechat_open_id = ?';
+    const apmtQuery = 'UPDATE appointments SET checked_in = ? WHERE id = ?';
+    const checkOutQuery = 'INSERT INTO check_outs (appointment_id, employee_id) VALUES (?, ?)';
+    
+    // results
+    pool.query(employeeIdQuery, [body.openId], (err1, results1) => {
+        if (err1) {
+            console.log('db error while fetching employee ID ' + body.openId);
+            return res.status(500).json({ error: 'db error while fetching employee ID' });
+        }
+        if (results1.length == 0) {
+            console.log('no employee found with openid ' + body.openId);
+            return res.status(500).json({ error: 'no employee foun with given openid' });
+        }
+        if (results1.length > 1) {
+            console.log('something is srsly fucked. why are there multiple employees for openid ' + body.openId);
+            console.log(results1);
+            return res.status(500).json({ error: 'something is srsly fucked. why are there multiple employees for the given openid '});
+
+        }
+        const employeeId = results1[0].id;
+        pool.query(apmtQuery, [false, body.apmtId], (err2, results2) => {
+            if (err2) {
+                console.log('db error while updating appointment id ' + body.apmtId);
+                return res.status(500).json({ error: `db error while updating appointment id ${body.apmtId}` });
+            }
+            pool.query(checkOutQuery, [body.apmtId, employeeId], (err3, results3) => {
+                if (err3) {
+                    console.log(`db error while creating check-out for apmtId ${body.apmtId}`);
+                    return res.status(500).json({ error: `db error while creating check-out for apmtId ${body.apmtId}` });
+                }
+                res.status(200).json({ message: 'appointment checked out'});
+            });
+        });
+    });
+};
--- routes/appointments.js
+++ routes/appointments.js
@@ -5,4 +5,10 @@ const appointmentsController = require('../controllers/appointmentsController');
 // Define the route to fetch appointment records
 router.get('/', appointmentsController.getAppointments);
 
+// checkin
+router.post('/checkin', appointmentsController.postCheckIn);
+
+// checkout
+router.post('/checkout', appointmentsController.postCheckOut);
+
 module.exports = router;
