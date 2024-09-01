4112828245fd6e707d00672958f056e4b749c100
Brian Lee <48812505+BrianYLee@users.noreply.github.com>
2024-09-01 20:59:42 +0800

    update endpoints

--- routes/register.js
+++ routes/register.js
@@ -5,7 +5,7 @@ const registerController = require('../controllers/registerController');
 router.get('/', registerController.getRegister);
 router.post('/', registerController.postRegister);
 router.get('/applications', registerController.getApplications);
-router.post('/application/approve', registerController.approve);
-router.post('/application/reject', registerController.reject);
+router.post('/approve', registerController.approve);
+router.post('/reject', registerController.reject);
 
 module.exports = router;
